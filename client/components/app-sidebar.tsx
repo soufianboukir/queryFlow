8e4e2a28fcf823e57885bc4e20e7f9e14038bf1b
soufian <soufianeboukir0@gmail.com>
2025-11-22 17:51:51 +0100

    fix: deployment issues

--- client/components/app-sidebar.tsx
+++ client/components/app-sidebar.tsx
@@ -125,7 +125,7 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
         <NavSecondary items={data.navSecondary} className="mt-auto" />
       </SidebarContent>
       <SidebarFooter>
-        <NavUser user={data.user} />
+        <NavUser />
       </SidebarFooter>
       <SidebarRail />
     </Sidebar>
