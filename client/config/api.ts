49d99cd2eae1d54810408b8dd756f45b6e8325c0
soufian <soufianeboukir0@gmail.com>
2025-11-28 19:52:01 +0100

    >>

--- client/config/api.ts
+++ client/config/api.ts
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000',
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:5000",
   withCredentials: true,
   headers: {
-    'Content-Type': 'application/json',
+    "Content-Type": "application/json",
   },
 });
--- client/services/auth.ts
+++ client/services/auth.ts
@@ -2,24 +2,22 @@ import { api } from "@/config/api";
 
 // Oauth with google
 export const login = async () => {
-  const response = await api.get(`/api/auth/google`);
-
+  const response = await api.get(`/auth/google`);
   return response;
 };
 
 // fetch current user
 export const getCurrentUser = async (token: string) => {
-  const response = await api.get(`/api/auth/me`, {
+  const response = await api.get(`/auth/me`, {
     headers: {
       Authorization: `Bearer ${token}`,
     },
   });
-  console.log("hhhhhh");
   return response;
 };
 
 export const logout = async () => {
-  await api.post("/api/auth/logout");
+  await api.post("/auth/logout");
   document.cookie = "token=; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT";
   window.location.href = "/";
 };
